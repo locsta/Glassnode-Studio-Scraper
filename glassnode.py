e0a76e7c255831c6d0fd5af8166bc3ba4eeea542
Marc Inizan <locstaw@gmail.com>
2021-01-25 15:53:48 +0000

    Fixed section name and removed useless librairies

--- glassnode.py
+++ glassnode.py
@@ -2,19 +2,13 @@ from selenium_scraper import SeleniumClass
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 import time
-from datetime import datetime
-import os
-import pandas as pd
 import json
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
-from pprint import pprint
-from itertools import cycle
 import pyautogui
-import threading
 
 def scrape_glassnode_studio():
     login_email = input("Please enter your email address:")
@@ -22,13 +16,13 @@ def scrape_glassnode_studio():
     url = "https://studio.glassnode.com/metrics?a=BTC&m=addresses.ActiveCount"
     browser = SeleniumClass(headless=False).open_browser()
     browser.get(url)
-    WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'ant-btn-link'))).click()
+    time.sleep(5)
+    browser.find_element_by_class_name("ant-btn-link").click()
     browser.find_element_by_id("loginForm_email").send_keys(login_email)
     browser.find_element_by_id("loginForm_password").send_keys(login_password)
     time.sleep(1)
     browser.find_element_by_class_name("ant-btn-primary").click()
     list_sections = {}
-    # Wait for the page to load fully TODO: look for a specific element present when the page is fully loaded and sue WebDriverWait instead of time.sleep(8)
     time.sleep(8)
     couldn_scrape = []
     for i in range(3, 21, 1):
@@ -43,7 +37,7 @@ def scrape_glassnode_studio():
         sections = browser.find_elements_by_class_name("ant-collapse-content-box")
         len_sections = len(sections)
         for j in range(len_sections):
-            section_name = sections[j].find_element_by_xpath('..').text.split("\n")[1]
+            section_name = sections[j].find_element_by_xpath('..').find_element_by_xpath('..').find_element_by_class_name('ant-collapse-header').text
             list_sections[category_name][section_name] = []
             print("section_name: " + section_name)
             csv_pages = sections[j].find_elements_by_tag_name("a")
